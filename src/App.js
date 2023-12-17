78c901de10dd102407d0723bea9a78d3e2267594
Tlek <tlekonay04@xmail.com>
2023-12-17 19:50:30 +0600

    цикл

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import Card from './components/Card';
+import Card from './components/Card/Card';
 import Header1 from './components/Header1';
 import Drawer from './components/Drawer';
 
+
+const arr = [
+    {
+        title: 'Men Vans Checker Logo Graphic Cuffed Beanie',
+        price: 5990,
+        imageUrl: '/img/shapki/1.jpg',
+    },
+    {
+        title: 'Men Dockers Sherpa Lined Rib Knit Cuff Beanie',
+        price: 2990,
+        imageUrl: '/img/shapki/2.jpg',
+    },
+]
+
 function App() {
   return (
-    
-    <div className="wrapper clear">
+        <div className="wrapper clear">
+            <Drawer/>
+            <Header1/>
+            <div className="content p-40">
+                  <div className="d-flex align-center justify-between mb-40">
+                    <h1>Все шапки</h1>
+                        <div className="search-block d-flex">
+                          <img className="mt-20" width={20} height={20} src="/img/search.png" alt="Search"/>
+                          <input placeholder="Поиск.." />
+                        </div>
+                  </div>
+                  <div className="d-flex">
+                       {arr.map((obj) => (
+                           <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl}
+                            onClick = {()=> console.log(obj)}
+                           />
+                       ))}
 
-        <Drawer/>
-        <Header1/>
 
-        <div className="content p-40">
-              <div className="d-flex align-center justify-between mb-40">
-                <h1>Все шапки</h1>
-                    <div className="search-block d-flex">
-                      <img className="mt-20" width={20} height={20} src="/img/search.png" alt="Search"/>
-                      <input placeholder="Поиск.." />
-                    </div>
-              </div>
-
-                <div className="d-flex">
-                    <Card/>
-                </div>
+                  </div>
+            </div>
         </div>
-    </div>
   );
 }
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,26 @@
-function Card(){
+import cardStyles from './Card.module.scss'
+
+function Card(props){
+    console.log(props);
+
     return(
-        <div className="card">
+        <div className={cardStyles.card}>
             <div className="favorite">
                 <img src="/img/unliked.svg" alt="Unliked" /> 
             </div> 
-                <img width={133} height={112} src="/img/shapki/1.jpg" alt="shapki" />
-                <h5>Men's Vans®  Graphic Cuffed Beanie</h5>
+                <img width={133} height={112} src={props.imageUrl} alt="shapki" />
+                <h5>{props.title}</h5>
                 <div className="d-flex justify-between align-center">
                     <div className="d-flex flex-column">
                         <span>Цена:</span>
-                        <b>5990 kzt</b>
+                        <b>{props.price} kzt</b>
                     </div>
-                <button className="button">
+                <button className="button" onClick={props.onClick}>
                     <img width={11} height={11} src="/img/plus333.png" alt="Plus" />
                 </button>
             </div>
         </div>
-  );
+     );
 }
 
 export default Card;
